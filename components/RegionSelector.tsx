a641243d4b95ada9d279a9c488c3b70bca7b3909
thatappguy71 <parryj1971@gmail.com>
2025-08-25 08:06:45 -0600

    Updated RegionSelector.tsx

--- components/RegionSelector.tsx
+++ components/RegionSelector.tsx
@@ -1,8 +1,27 @@
-@@ .. @@
- import { regions, type Region } from '../lib/regions';
- 
--export default function RegionSelector({ 
-+export function RegionSelector({ 
-   selectedRegion, 
-   onRegionChange 
- }: {
+import { regions, type Region } from '../lib/regions';
+
+export function RegionSelector({ 
+  selectedRegion, 
+  onRegionChange 
+}: {
+  selectedRegion: Region;
+  onRegionChange: (region: Region) => void;
+}) {
+  return (
+    <select 
+      value={selectedRegion.code} 
+      onChange={(e) => {
+        const region = regions.find(r => r.code === e.target.value);
+        if (region) {
+          onRegionChange(region);
+        }
+      }}
+    >
+      {regions.map(region => (
+        <option key={region.code} value={region.code}>
+          {region.name}
+        </option>
+      ))}
+    </select>
+  );
+}
