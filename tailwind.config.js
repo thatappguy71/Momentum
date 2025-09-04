b767770b0644b80a9f3f2553e7d886920f18e647
John Parry <parryj1971@gmail.com>
2025-09-04 17:02:44 -0600

    Fix PostCSS syntax error with proper Tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'primary-foreground': 'hsl(var(--primary-foreground))',
         primary: {
           50: '#f0f9ff',
           100: '#e0f2fe',
